766f8ce9b5904ed6dc2c48d97bbf6a4b5c90ea7e
ventu-ra <ventura09.10.0@gmail.com>
2025-08-10 17:30:40 -0300

    Refactor getRestaurants method to return a paginated list of RestaurantDTOs

--- src/main/java/dev/ventura/meat_backend_with_spring/service/RestaurantService.java
+++ src/main/java/dev/ventura/meat_backend_with_spring/service/RestaurantService.java
@@ -1,10 +1,13 @@
 package dev.ventura.meat_backend_with_spring.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import dev.ventura.meat_backend_with_spring.dto.RestaurantDTO;
 import dev.ventura.meat_backend_with_spring.entity.Restaurant;
 import dev.ventura.meat_backend_with_spring.repository.RestaurantRepository;
 
@@ -18,8 +21,11 @@ public class RestaurantService {
     this.repository = repository;
   }
 
-  public List<Restaurant> getRestaurants() {
-    return repository.findAll();
+  public List<RestaurantDTO> getRestaurants() {
+    return repository.findAll(PageRequest.of(0, 5))
+        .stream()
+        .map(RestaurantDTO::listAllDto)
+        .collect(Collectors.toList());
   }
 
   public Restaurant save(Restaurant restaurant) {
